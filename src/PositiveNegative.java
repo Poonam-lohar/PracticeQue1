ccf2ae488a5addc09f0b4670a1d8a84f19eeb8ff
Your Name <you@example.com>
2022-08-16 15:44:52 +0530

    added:Welcome

---
+++ src/PositiveNegative.java
@@ -0,0 +1,12 @@
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        int num = -2;
+        if (num > 0) {
+            System.out.println("munber is positive");
+        } else if (num < 0) {
+            System.out.println("number is negative");
+        } else
+            System.out.println("invalid");
+    }
+}
