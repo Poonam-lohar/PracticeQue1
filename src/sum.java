bb2e4e6098bf5504a487026dd7fe34730d7d127a
Your Name <you@example.com>
2022-08-16 16:09:45 +0530

    logic for sum of numbers in given range

---
+++ src/sum.java
@@ -0,0 +1,14 @@
+public class sum {
+        public static void main (String[]args)
+        {
+            int a = 3;
+            int b = 10;
+
+            int sum = 0;
+
+            for (int i = a; i <= b; i++)
+                sum = sum + i;
+            System.out.println ("The sum is " + sum);
+        }
+
+    }
